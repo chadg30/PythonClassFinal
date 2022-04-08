faddba131d15b977fcb823507cfd0f0efe6c2318
michaelelias <mwe1002@sru.edu>
2022-04-08 18:20:12 -0400

    top level

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
 # Python Final Project - Part 1
 # Elijah McClymonds, Mike Elias, Chad Green
 
+from time import perf_counter
+
+
 def main():
+    pass
 
 
+def validate_data(first_name, last_name, student_id):
+    # 3 attempts max to validate
+    pass
 
-if __name__=="__main__":
+
+def generate_questions():
+    # randomly pick 10 questions from csv file
+    pass
+
+
+def display_question():
+    #check for input validation for answers
+    pass
+
+
+def toggle_timer():
+    #toggle between start and stop timer
+    pass
+
+def record_data():
+    #write statistics to text file
+    pass
+
+
+if __name__ == "__main__":
     main()
