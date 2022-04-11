c24a7113092de60c931f34df70a960fc3abd3cb8
chadg30 <cwg1003@sru.edu>
2022-04-11 13:30:15 -0400

    Validate Data

--- part1/main.py
+++ part1/main.py
@@ -5,11 +5,20 @@ from time import perf_counter
 
 
 def main():
-    print("Pain\n")
+    print("Python Quiz")
+    valid = False
+    count = 0
     while True:
-        first_name = input("Enter your First Name: ")
-        last_name = input("Enter your Last Name: ")
-        student_id = input("Enter your Student ID: ")
+        # get and validate user info, User gets 3 attempts
+        while not valid and count < 3:
+            first_name = input("\nEnter your First Name: ")
+            last_name = input("Enter your Last Name: ")
+            student_id = input("Enter your Student ID: ")
+            valid = validate_data(first_name, last_name, student_id, valid)
+            count += 1
+        if count == 3:
+            print("Too Many Login Attempts")
+            break
 
         # finishing
         user_input = input("Do you want to quit or start again? (q/s): ").lower()
@@ -17,37 +26,71 @@ def main():
             break
         elif user_input == "s":
             print("Restarting...")
+            valid = False
+            count = 0
         # exception for input?
     print("Bye!")
 
 
-#chad man dude bro
-def validate_data(first_name, last_name, student_id):
+# chad man dude bro
+def validate_data(first_name, last_name, student_id, valid):
     # 3 attempts max to validate
-    pass
+    count = 0
+
+    if first_name.strip().isalpha():  # check if name is alpha
+        valid = True
+    else:
+        print("Invalid Name Entered, Try Again")
+        return False
+    if last_name.strip().isalpha():  # check if last name is alpha
+        valid = True
+    else:
+        print("Invalid Name Entered, Try Again")
+        return False
+    if len(student_id) == 6:  # check student ID length
+        valid = True
+    else:
+        print("Student ID incorrect length, Try Again")
+        return False
+    if student_id[0] == 'A':  # check if student ID starts with A
+        valid = True
+    else:
+        print("Student ID incorrect, Try Again")
+        return False
+    if int(student_id[1:5].isdigit()):  # check if end of ID is numerical
+        for char in student_id[1:5]:  # check if each digit in end of ID is between 1 and 9
+            if 1 <= int(char) <= 9:
+                valid = True
+            else:
+                print("Invalid Student ID, Try Again")
+                return False
+    else:
+        print("Invalid Student ID, Try Again")
+
+    return valid
 
 
-#elijah md
+# elijah MidClymonds
 def generate_questions():
     # randomly pick 10 questions from csv file
     pass
 
 
-#mike man
+# mike man
 def display_question():
-    #check for input validation for answers
+    # check for input validation for answers
     pass
 
 
-#chad man dude bro
+# chad man dude bro
 def toggle_timer():
-    #toggle between start and stop timer
+    # toggle between start and stop timer
     pass
 
 
-#mike man
+# mike man
 def record_data():
-    #write statistics to text file
+    # write statistics to text file
     pass
 
 
